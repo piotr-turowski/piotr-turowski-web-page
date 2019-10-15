14760e80062e7ddd0a8ee0dc6d92ecaad4850995
piotr-turowski <piotr.turowski5@gmail.com>
2019-10-15 02:13:29 +0200

    add new features

---
+++ script.js
@@ -0,0 +1,23 @@
+//settings
+var icon = document.querySelector('.icon');
+var topnav = document.querySelector('.topnav');
+var sticky = topnav.offsetTop;
+
+
+
+//listeners
+icon.addEventListener('click', function () {
+    if (topnav.className === 'topnav') {
+        topnav.className += ' responsive';
+    } else {
+        topnav.className = 'topnav';
+    }
+});
+
+window.addEventListener('scroll',function(){
+    if(window.pageYOffset > sticky){
+    topnav.classList.add('sticky');
+    } else {
+    topnav.classList.remove('sticky');
+    }
+});
